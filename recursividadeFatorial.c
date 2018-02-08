5807f21ca253505366d981ad0ade0813ff3a4258
lucasmas16 <lucasmas16@gmail.com>
2018-02-08 18:46:16 -0500

    commit da versao de teste funcional

--- recursividadeFatorial.c
+++ recursividadeFatorial.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <math.h>
 int fatorialIterativo(int n){
   int resp = 1;
   int i;
@@ -9,4 +11,15 @@ int fatorialIterativo(int n){
 int fatorialRecursivo(int n){
   if(n <= 1) return 1;
   return n * fatorialRecursivo(n - 1);
+}
+int main(void) {
+	setbuf(stdout, NULL);
+  
+  int n;
+	printf("\nDigite número para digitar o fatorial: ");
+	fflush(stdin);
+	scanf("%d", &n);
+  printf("O fatorial iterativo é: %d\nO fatorial recursivo é: %d", fatorialIterativo(n), fatorialRecursivo(n));
+  printf("\nSucesso\n");
+  return 0;
 }
