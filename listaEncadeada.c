7a3309bf408361f667a0afb25df1d8c0c9660fac
lucasmas16 <lucasmas16@gmail.com>
2018-02-13 08:02:02 -0500

    Insercao de menu de funcoes basicas, ainda nao entendi o metodo de retirada demostrado no livro

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct Celula_str *Apontador;
 
 typedef struct {
@@ -27,28 +30,70 @@ void Insere(TipoItem x, TipoLista *Lista){
   Lista->Ultimo->Prox = (Apontador) malloc(sizeof(Celula));
   Lista->Ultimo = Lista->Ultimo->Prox;
   Lista->Ultimo->Item = x;
-  Lista->Ultimo-Prox = NULL;
+  Lista->Ultimo->Prox = NULL;
 }
-
-void Retira(Apontador p, TipoLista *Lista, TipoItem *Item){
-  if(Vazia(*Lista) || p == NULL || p->Prox == NULL){
+/*
+void Retira(TipoLista *lista){
+  Apontador p = lista.Primeiro;
+  TipoItem *i = lista.Primeiro.Item;
+  Apontador q;
+  if(Vazia(*lista) || p == NULL || p->Prox == NULL){
     printf("Erro: Lista vazia ou nao existe \n");
     return;
-  }q = p->Prox;
-  *Item = q->Item;
+  }
+  q = p->Prox;
+  i = q->Item;
   p->Prox = q->Prox;
   if(p->Prox == NULL){
     Lista->Ultimo = p;
   }
   free(q);
-}
+}*/
 
 void Imprime(TipoLista Lista){
   Apontador Aux;
   Aux = Lista.Primeiro->Prox;
   while(Aux != NULL){
-    printf("%12ld\n", Aux->Item.Chave);
+    printf("%d\n", Aux->Item.Chave);
     Aux = Aux->Prox;
   }
+}
+
+int main(void){
+  setbuf(stdout, NULL);
+  int choice;
+  TipoItem item;
+  TipoLista lista;
+  TipoLista *plista = &lista;
+  EsvaziarLista(plista);
+
+  do{
+    printf("Inserir item na lista          - 1\n");
+    printf("Remover primeiro item da lista - 2\n");
+    printf("Esvasiar a lista               - 3\n");
+    printf("Imprimir a lista               - 4\n");
+    printf("Sair do programa               - 0\n");
+    printf("Digite a opcao desejada:");
+    fflush(stdin);
+  	scanf("%d", &choice);
+    switch(choice){
+      case 1:
+        printf("Digite um numero para inserir na lista: ");
+        fflush(stdin);
+        scanf("%d", &item.Chave);
+        Insere(item, plista);
+        break;
+      case 2:
+        //Retira(plista);
+        break;
+      case 3:
+        EsvaziarLista(plista);
+        if(Vazia(lista))printf("Lista vazia\n");
+        break;
+      case 4:
+        Imprime(lista);
+        break;
+    }
+  }while(choice != 0);
   
 }
