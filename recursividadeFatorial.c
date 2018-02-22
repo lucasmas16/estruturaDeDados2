8cee4d38a8022101c5d15d700da08ba67eb3b722
Lucas Santos <lucasmas16@gmail.com>
2018-02-21 22:04:15 -0300

    Adição do metodo de tail.

--- recursividadeFatorial.c
+++ recursividadeFatorial.c
@@ -8,18 +8,29 @@ int fatorialIterativo(int n){
   }
   return resp;
 }
+
+long fatorialTail(int n, long rslt){
+  if(n==0)return rslt;
+  return fatorialTail(n-1, n * rslt);
+}
+
+long fatTail(int n){
+  return fatorialTail(n, 1);
+}
+
 int fatorialRecursivo(int n){
   if(n <= 1) return 1;
   return n * fatorialRecursivo(n - 1);
 }
 int main(void) {
 	setbuf(stdout, NULL);
-  
+
   int n;
 	printf("\nDigite número para digitar o fatorial: ");
 	fflush(stdin);
 	scanf("%d", &n);
-  printf("O fatorial iterativo é: %d\nO fatorial recursivo é: %d", fatorialIterativo(n), fatorialRecursivo(n));
+  printf("O fatorial iterativo é: %d\nO fatorial recursivo é: %d \n", fatorialIterativo(n), fatorialRecursivo(n));
+  printf("O fatorial de calda é: %ld", fatTail(n));
   printf("\nSucesso\n");
   return 0;
 }
