4efccaebfa96f83b11b1ba881d08da2ed92b6b96
Lucas Santos <lucasmas16@gmail.com>
2018-03-05 11:48:04 -0300

    Pequenos ajustes e comentários

--- modeloDePilha/pilhaModelo.c
+++ modeloDePilha/pilhaModelo.c
@@ -1,12 +1,15 @@
+//define estrutura de Celula simple com um char
 typedef struct{
   char item;
   Celula *next;
 } Celula;
 
+//define estrutura de Pilha
 typedef struct{
   Celula topo;
 } Pilha;
 
+//insere elemento no começo da pilha
 void push(Pilha *pilha, char newItem){
   Celula new = (Celula)malloc(sizeof(Celula));
   new->item = newItem;
@@ -14,13 +17,14 @@ void push(Pilha *pilha, char newItem){
   pilha->topo = new;
 }
 
-Celula pop(Pilha *pilha){
-  Celula new = (Celula)malloc(sizeof(Celula));
+//desempilha o primeiro elemento
+void pop(Pilha *pilha, Celula *new){
+  new = (Celula)malloc(sizeof(Celula));
   new = pilha->topo;
   pilha->topo = pilha->topo->next;
-  return new;
 }
 
+//retorna o conteudo do primeiro elemento para efetuar comparações
 char watch(Pilha *pilha){
   return pilha->item;
 }
