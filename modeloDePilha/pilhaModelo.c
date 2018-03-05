601f47cc3ce28a15a1049baecf7903bf19b69d5b
lucasmas16 <lucasmas16@gmail.com>
2018-03-05 13:51:09 -0500

    Correcao de bugs na estrutura e inclusao de comentarios

--- modeloDePilha/pilhaModelo.c
+++ modeloDePilha/pilhaModelo.c
@@ -1,30 +1,44 @@
-//define estrutura de Celula simple com um char
-typedef struct{
-  char item;
-  Celula *next;
+#include <stdio.h>
+#include <stdlib.h>
+
+//define que o tipo de item da pilha será Char
+typedef struct {
+  char chave;
+}Item;
+
+//Define um ponteiro para poder referênciar dentro da estrutura da célula
+typedef struct Cel *Apontador;
+
+//Estrutura de Célula
+typedef struct Cel{
+  Item item;//item da célula | pode ser alterado para qualquer outro tipo de estrutura
+  Apontador next;//ponteiro para a proxima célula
 } Celula;
 
-//define estrutura de Pilha
-typedef struct{
-  Celula topo;
+
+//Estrutura de Pilha usando apenas variável do topo
+typedef struct Pilha{
+  Apontador topo;
 } Pilha;
 
 //insere elemento no começo da pilha
-void push(Pilha *pilha, char newItem){
-  Celula new = (Celula)malloc(sizeof(Celula));
-  new->item = newItem;
-  new->next = pilha->topo;
-  pilha->topo = new;
+void push(Pilha *pilha, Item newItem){
+  Apontador nova = (Apontador)malloc(sizeof(Celula));//inicia um novo ponteiro alocando o espaço para uma célula
+  nova->item = newItem;//item da nova célula recebe o item do parâmetro
+  nova->next = pilha->topo;//linka o próximo do novo elemento com o topo da pilha
+  pilha->topo = nova;//transfere o topo da pilha para o novo elemento
 }
 
 //desempilha o primeiro elemento
-void pop(Pilha *pilha, Celula *new){
-  new = (Celula)malloc(sizeof(Celula));
-  new = pilha->topo;
-  pilha->topo = pilha->topo->next;
+void pop(Pilha *pilha, Apontador new){
+  new = (Apontador)malloc(sizeof(Celula));//inicializa o novo elemento
+  new = pilha->topo;//o novo elemento recebe o topo
+  if(topo != NULL){//verifica se a pilha não está vazia
+    pilha->topo = pilha->topo->next;//o topo passa a ser o segundo elemento
+  }
 }
 
 //retorna o conteudo do primeiro elemento para efetuar comparações
 char watch(Pilha *pilha){
-  return pilha->item;
+  return pilha->topo->item.chave;
 }
