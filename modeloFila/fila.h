ae40e47fa6b9845336472ebe775023b2c3f2dfbe
Lucas Santos <lucasmas16@gmail.com>
2018-03-21 14:35:54 -0300

    Criação de estrutura de fila

---
+++ modeloFila/fila.h
@@ -0,0 +1,20 @@
+//define que o tipo de item da fila
+typedef struct {
+  char chave;
+}Item;
+
+//Define um ponteiro para poder referênciar dentro da estrutura da célula
+typedef struct Cel *PONT;
+
+//Estrutura de Célula
+typedef struct Cel{
+  Item item;//item da célula | pode ser alterado para qualquer outro tipo de estrutura
+  PONT next;//ponteiro para a proxima célula
+} Celula;
+
+
+//Estrutura de fila usando variável topo, fim e tamanho
+typedef struct Fila{
+  PONT start, end;
+  int size;
+} Pilha;
